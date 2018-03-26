8bb679daa3258a02bcf402fd14e4fe1c9d75c769
Robialk <carlos.micael@iol.pt>
2018-03-26 21:37:34 +0100

    GPS (latitude e logitude)

--- app/src/main/java/com/example/carlo/myapplication/MainActivity.java
+++ app/src/main/java/com/example/carlo/myapplication/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.carlo.myapplication;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.AsyncQueryHandler;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,16 +12,19 @@ import android.hardware.SensorManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
 
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 
+import android.util.Log;
 import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 
 
-public class MainActivity extends AppCompatActivity implements SensorEventListener {
+public class MainActivity extends AppCompatActivity implements SensorEventListener, LocationListener{ //LocationListener
 
     Sensor accelerometer;
     SensorManager sensorManager;
@@ -26,20 +32,20 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     float sensorY;
     float sensorZ;
 
+    TextView tGps;
+
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)  {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sensorManager =(SensorManager) getSystemService(SENSOR_SERVICE);
-        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
 
-        //DATA E HORAS
-        Thread t = new Thread(){
+        //****************************************DATA E HORAS********************************************************
+        Thread t = new Thread() {
             @Override
-            public void run(){
-                try{
-                    while (!isInterrupted()){
+            public void run() {
+                try {
+                    while (!isInterrupted()) {
                         Thread.sleep(1000);
                         runOnUiThread(new Runnable() {
                             @Override
@@ -52,14 +58,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
                             }
                         });
                     }
-                }catch (InterruptedException e){
+                } catch (InterruptedException e) {
 
                 }
             }
         };
         t.start();
-    }
 
+        //****************************************ACELEROMETRO********************************************************
+        sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+
+        //****************************************GPS (LOCALIZAçÃO)***************************************************
+        tGps=findViewById(R.id.tGPS);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION
+                }, 1234);
+                return;
+            }
+        }
+
+    }
+    //****************************************ACELEROMETRO**********************************************************
     @Override
     public void onSensorChanged(SensorEvent event) {
         sensorX = event.values[0];
@@ -94,4 +119,55 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     }
 
+
+
+
+    //****************************************GPS LOCALIZAçÃO********************************************************
+    @Override
+    protected void onResume() {
+        super.onResume();
+        SensorManager sm =(SensorManager) getSystemService(SENSOR_SERVICE);
+        LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+            if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
+                    checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,this);
+    }
+
+    @SuppressLint("MissingPermission")
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode==1234 &&
+                grantResults[0]==PackageManager.PERMISSION_GRANTED &&
+                grantResults[1]==PackageManager.PERMISSION_GRANTED){
+            LocationManager lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,10,10,this); //toDo: actualizar valores
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        double longitude = location.getLongitude();
+        double latitude = location.getLatitude();
+        tGps.setText("GPS: "+ longitude + " , " + latitude);
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
 }
